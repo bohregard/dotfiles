c1ca09bd6831e362dd92ab0dbd95eda8a61c547f
Anthony Todd <awtodd89@gmail.com>
2024-03-14 00:04:19 -0400

    Set file permissions and capture script dir for portability

--- init.sh
+++ init.sh
@@ -1,26 +1,38 @@
 #!/bin/zsh
 
+SCRIPT_DIR=${0:a:h}
 ZSH="$HOME/.zshrc"
 P10K="$HOME/.p10k.zsh"
-ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
+OH_MY_ZSH_ROOT="$HOME/.oh-my-zsh"
+OH_MY_ZSH_CUSTOM="$OH_MY_ZSH_ROOT/custom"
+OH_MY_ZSH_PLUGINS="$OH_MY_ZSH_CUSTOM/plugins"
 TMUX="$HOME/.tmux.conf"
+
+echo "=> Checking Symlinks"
 
 if [[ ! -h "$ZSH" ]]; then
     echo "=> Symlinking zshrc to $ZSH"
-    ln -s "$(pwd)/zshrc" "$ZSH"
+    ln -s "$SCRIPT_DIR/zshrc" "$ZSH"
 fi
 
 if [[ ! -h "$P10K" ]]; then
     echo "=> Symlinking p10k.zsh to $P10K"
-ln -s "$(pwd)/p10k.zsh" "$P10K"
+    ln -s "$SCRIPT_DIR/p10k.zsh" "$P10K"
 fi
 
-if [[ ! -h "$ZSH_CUSTOM" ]]; then
-    echo "=> Symlinking oh-my-zsh/custom to $ZSH_CUSTOM"
-ln -s "$(pwd)/oh-my-zsh/custom" "$ZSH_CUSTOM"
+if [[ ! -h "$OH_MY_ZSH_CUSTOM" ]]; then
+    echo "=> Symlinking oh-my-zsh/custom to $OH_MY_ZSH_CUSTOM"
+    ln -s "$SCRIPT_DIR/oh-my-zsh/custom" "$OH_MY_ZSH_CUSTOM"
 fi
 
 if [[ ! -h "$TMUX" ]]; then
     echo "=> Symlinking .tmux.conf to $TMUX"
-ln -s "$(pwd)/tmux.conf" "$TMUX"
+    ln -s "$SCRIPT_DIR/tmux.conf" "$TMUX"
 fi
+
+echo "=> Setting Directory permissions"
+
+chmod 755 $OH_MY_ZSH_PLUGINS/zsh-autosuggestions
+chmod 755 $OH_MY_ZSH_PLUGINS/zsh-syntax-highlighting
+chmod 755 $OH_MY_ZSH_PLUGINS
+chmod 755 $OH_MY_ZSH_ROOT/cache/completions
