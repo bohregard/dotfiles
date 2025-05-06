f0ae28ec693bef7b87f8e01fac74118f951d5951
bohregard <awtodd89@gmail.com>
2025-05-06 09:41:16 -0400

    Use custom plugin directory for zsh

--- init.sh
+++ init.sh
@@ -4,8 +4,8 @@ SCRIPT_DIR=${0:a:h}
 ZSH="$HOME/.zshrc"
 P10K="$HOME/.p10k.zsh"
 OH_MY_ZSH_ROOT="$HOME/.oh-my-zsh"
-OH_MY_ZSH_CUSTOM="$OH_MY_ZSH_ROOT/custom"
-OH_MY_ZSH_PLUGINS="$OH_MY_ZSH_CUSTOM/plugins"
+# OH_MY_ZSH_CUSTOM="$OH_MY_ZSH_ROOT/custom"
+# OH_MY_ZSH_PLUGINS="$OH_MY_ZSH_CUSTOM/plugins"
 TMUX="$HOME/.tmux.conf"
 
 echo "=> Checking Symlinks"
@@ -20,10 +20,10 @@ if [[ ! -h "$P10K" ]]; then
     ln -s "$SCRIPT_DIR/p10k.zsh" "$P10K"
 fi
 
-if [[ ! -h "$OH_MY_ZSH_CUSTOM" ]]; then
-    echo "=> Symlinking oh-my-zsh/custom to $OH_MY_ZSH_CUSTOM"
-    ln -s "$SCRIPT_DIR/oh-my-zsh/custom" "$OH_MY_ZSH_CUSTOM"
-fi
+# if [[ ! -h "$OH_MY_ZSH_CUSTOM" ]]; then
+#     echo "=> Symlinking oh-my-zsh/custom to $OH_MY_ZSH_CUSTOM"
+#     ln -s "$SCRIPT_DIR/oh-my-zsh/custom" "$OH_MY_ZSH_CUSTOM"
+# fi
 
 if [[ ! -h "$TMUX" ]]; then
     echo "=> Symlinking .tmux.conf to $TMUX"
@@ -32,7 +32,7 @@ fi
 
 echo "=> Setting Directory permissions"
 
-chmod 755 $OH_MY_ZSH_PLUGINS/zsh-autosuggestions
-chmod 755 $OH_MY_ZSH_PLUGINS/zsh-syntax-highlighting
-chmod 755 $OH_MY_ZSH_PLUGINS
-chmod 755 $OH_MY_ZSH_ROOT/cache/completions
+# chmod 755 $OH_MY_ZSH_PLUGINS/zsh-autosuggestions
+# chmod 755 $OH_MY_ZSH_PLUGINS/zsh-syntax-highlighting
+# chmod 755 $OH_MY_ZSH_PLUGINS
+# chmod 755 $OH_MY_ZSH_ROOT/cache/completions
